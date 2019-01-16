f30c533c55f9019559f5456c3f3764dcbab9f8ab
jbcastro <josephbeckcastro@gmail.com>
2019-01-16 13:59:57 -0800

    second commit, CRUD working however I'm working on how to store an arrray. Added an array in the postReducer, next step is to edit it to look like my CRUD for episodes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,17 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 import postReducer from "./reducers/postReducer";
+
 const store = createStore(postReducer);
+
+const butt = [
+  {
+    editing: "false",
+    id: "Wed Jan 16 2019 13:30:28 GMT-0800 (Pacific Standard Time)",
+    message: "things",
+    title: "stuff"
+  }
+];
 ReactDOM.render(
   <Provider store={store}>
     <App />
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,7 +1,34 @@
-const postReducer = (state = [], action) => {
+const postReducer = (
+  state = [
+    {
+      editing: "true",
+      id: "Wed Jan 16 2019 13:30:28 GMT-0800 (Pacific Standard Time)",
+      message: "things",
+      title: "stuff"
+    }
+  ],
+  action
+) => {
   switch (action.type) {
     case "ADD_POST":
       return state.concat([action.data]);
+    case "DELETE_POST":
+      return state.filter(post => post.id !== action.id);
+    case "EDIT_POST":
+      return state.map(post =>
+        post.id === action.id ? { ...post, editing: !post.editing } : post
+      );
+    case "UPDATE":
+      return state.map(post => {
+        if (post.id === action.id) {
+          return {
+            ...post,
+            title: action.data.newTitle,
+            message: action.data.newMessage,
+            editing: !post.editing
+          };
+        } else return post;
+      });
     default:
       return state;
   }
